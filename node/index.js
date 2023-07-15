b0ac80889e0847c5bdcb0e9cd7dda40fa143df27
hugodnpm <hugosc@gmail.com>
2023-07-14 21:01:12 -0300

    fix docker-compose mysql and node

--- node/index.js
+++ node/index.js
@@ -9,52 +9,40 @@ const config = {
   database: "nodedb",
 };
 const mysql = require("mysql");
-const connection = mysql.createConnection(config);
-const sql = `INSERT INTO people(name) values('Hugo')`;
-const findAll = `SELECT * FROM people LIMIT 10`;
+const connect = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) values('Hugo Carvalho')`;
+connect.query(sql);
 
-const executeSQL = (sql) => {
-  return new Promise((resolve, reject) => {
-    connection.query(sql, (error, results, fields) => {
-      if (error) {
-        reject(error);
-      } else {
-        results = JSON.parse(JSON.stringify(results));
-        const result = `
-      <div>
-        <h1>Full Cycle Rocks!</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-            </tr>
-          </thead>
-          <tbody>
-            ${results
-              .map(
-                (row) =>
-                  `<tr>
-                    <td>${row.name}</td>
-                  </tr>`
-              )
-              .join("")}
-          </tbody>
-        </table>
-      </div>
-    `;
+app.get("/", (req, res) => {
+  const conn = mysql.createConnection(config);
+  conn.query("SELECT * FROM people", (error, results, fields) => {
+    const result = `
+    <div>
+      <h1>Full Cycle Rocks!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${results
+            .map(
+              (row) =>
+                `<tr>
+                  <td>${row.name}</td>
+                </tr>`
+            )
+            .join("")}
+        </tbody>
+      </table>
+    </div>
+  `;
 
-        resolve(result);
-      }
-      connection.end();
-    });
+    res.send(result);
   });
-};
-app.get("/", async (req, res) => {
-  const result = await executeSQL(findAll);
-
-  res.send(result);
+  connect.end();
 });
-
 app.listen(port, () => {
   console.log(`Rodando na porta => ${port}`);
 });
